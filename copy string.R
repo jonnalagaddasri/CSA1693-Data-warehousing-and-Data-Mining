5b8442d1c8047f6dc8055aa9fb6f08777e9845f2
jonnalagaddasri <112365618+jonnalagaddasri@users.noreply.github.com>
2023-05-04 13:33:39 +0530

    Add files via upload
    original_string <- "greetings from sse!!"
    > copy_string <- paste(original_string)
    > print(copy_string)
    [1] "greetings from sse!!"
    > original_string <- "Welcome to SSE family!!"
    > copy_string <- paste(original_string)
    > print(copy_string)
    [1] "Welcome to SSE family!!"

---
+++ copy string.R
@@ -0,0 +1,7 @@
+original_string <- "greetings from sse!!"
+copy_string <- paste(original_string)
+print(copy_string)
+
+original_string <- "Welcome to SSE family!!"
+copy_string <- paste(original_string)
+print(copy_string)
