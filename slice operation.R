22fc796c7dfc82d7ab84b0fbaf04e45e62c50451
jonnalagaddasri <112365618+jonnalagaddasri@users.noreply.github.com>
2023-05-04 13:42:39 +0530

    Add files via upload
     s <-"POOJASRI"
    > print(substring(s,1,1))
    [1] "P"
    > print(substring(s,1,5))
    [1] "POOJA"
    > my_string <- "Hello, world!"
    > 
    > slice <- substr(my_string, 1, 5)
    > print(slice)
    [1] "Hello"
    > 
    > df<-c("Afroz","Joseph","Hayati","Jelly","Palima","Mica")
    > substring(df,4,4)<-c("$")
    > df
    [1] "Afr$z"  "Jos$ph" "Hay$ti" "Jel$y"  "Pal$ma" "Mic$"  
    > 
    > 
    > df<-("Saveetha_school_of_Engineering")
    > substring(df,1,11)
    [1] "Saveetha_sc"

---
+++ slice operation.R
@@ -0,0 +1,15 @@
+s <-"POOJASRI"
+print(substring(s,1,1))
+print(substring(s,1,5))
+my_string <- "Hello, world!"
+
+slice <- substr(my_string, 1, 5)
+print(slice)
+
+df<-c("Afroz","Joseph","Hayati","Jelly","Palima","Mica")
+substring(df,4,4)<-c("$")
+df
+
+
+df<-("Saveetha_school_of_Engineering")
+substring(df,1,11)
